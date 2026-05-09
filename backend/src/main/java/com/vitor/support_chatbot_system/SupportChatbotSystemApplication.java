acaaf4601e023e66854897c525ad6c4c1aa63785
Vitor Aguiar <vitor.aguiar.8960@gmail.com>
2026-05-09 14:19:30 -0300

    chore: create backend architecture packages

---
+++ backend/src/main/java/com/vitor/support_chatbot_system/SupportChatbotSystemApplication.java
@@ -0,0 +1,13 @@
+package com.vitor.support_chatbot_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportChatbotSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportChatbotSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vitor/support_chatbot_system/SupportChatbotSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitor.support_chatbot_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportChatbotSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
